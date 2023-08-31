162e774318285a42cedfecf118e49451a2dc91e5
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-08-31 20:38:38 +0100

    new commponents

--- src/components/crmaLeeaci/views/CrmaLeeaciHome.js
+++ src/components/crmaLeeaci/views/CrmaLeeaciHome.js
@@ -1,6 +1,9 @@
 import Header from "../header/Header"
 import Presentation from "../presentation/Presentation"
 import Meditation from "../meditation/Meditation"
+import Evenements from "../evenement/Evenements"
+import TheologiePratique from "../theologie/TheologiePratique"
+import Footer from "../footer/Footer"
 
 function CrmaLeeaciHome() {
     return(
@@ -8,6 +11,9 @@ function CrmaLeeaciHome() {
             <Header />
             <Presentation />
             <Meditation />
+            <Evenements />
+            <TheologiePratique />
+            <Footer />
         </>
     )
 }
