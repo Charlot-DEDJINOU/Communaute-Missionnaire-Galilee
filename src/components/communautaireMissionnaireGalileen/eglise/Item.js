b83cb99715ea7a0a0ff5365b7444e5277cfe7127
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-09-27 10:55:09 +0100

    components Item

--- src/components/communautaireMissionnaireGalileen/eglise/Item.js
+++ src/components/communautaireMissionnaireGalileen/eglise/Item.js
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom"
+
 function Item(props) {
+
+    const navigate = useNavigate()
     
     return(
         <div style={{ backgroundImage: `url(${props.image})` }} className="d-flex flex-column align-items-center mb-5 item">
             <h3 className="my-5 text-white fw-bold">{props.title}</h3>
             <p className="text-white px-10" style={{fontSize : "13px"}}>{props.text}</p>
-            <button className="mt-3">{props.buttonValue}</button>
+            <button className="mt-3" onClick={() => navigate(props.url)}>{props.buttonValue}</button>
         </div>
     )
 }
