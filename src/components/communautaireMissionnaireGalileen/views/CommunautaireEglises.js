6930f8310e25a5e30e4054d1d5453e32681c606e
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-09-27 11:00:20 +0100

    page CommunautaireEglise

---
+++ src/components/communautaireMissionnaireGalileen/views/CommunautaireEglises.js
@@ -0,0 +1,15 @@
+import Header from "../header/Header"
+import Footer from "../footer/Footer"
+import Eglises from "../eglise/Eglises"
+
+function CommunautaireEglises() {
+    return(
+        <>
+            <Header />
+            <Eglises />
+            <Footer />
+        </>
+    )
+}
+
+export default CommunautaireEglises
