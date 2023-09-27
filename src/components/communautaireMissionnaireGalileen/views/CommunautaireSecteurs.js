95cba4fcea37487f0f6d6c1f082bd59b6505c919
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-09-27 11:02:10 +0100

    Page CommunautaireSecteur

---
+++ src/components/communautaireMissionnaireGalileen/views/CommunautaireSecteurs.js
@@ -0,0 +1,15 @@
+import Header from "../header/Header"
+import Footer from "../footer/Footer"
+import Secteurs from "../secteur/Secteurs"
+
+function CommunautaireSecteurs() {
+    return(
+        <>
+            <Header />
+            <Secteurs />
+            <Footer />
+        </>
+    )
+}
+
+export default CommunautaireSecteurs
