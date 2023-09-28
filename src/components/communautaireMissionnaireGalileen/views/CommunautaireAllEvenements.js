69b8facdd67c7f348e68725bc27f84fb5a5c95f7
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-09-28 14:17:56 +0100

    page all events

---
+++ src/components/communautaireMissionnaireGalileen/views/CommunautaireAllEvenements.js
@@ -0,0 +1,15 @@
+import AllEvenements from "../agenda/AllEvenements";
+import Header from "../header/Header"
+import Footer from "../footer/Footer"
+
+function CommunautaireAllEvenements() {
+    return(
+        <>
+            <Header />
+            <AllEvenements />
+            <Footer />
+        </>
+    )
+}
+
+export default CommunautaireAllEvenements
