c69ada77d9b40be0864c3d5dbe458f4ec0435bbe
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-09-28 14:18:44 +0100

    page evenements futurs

---
+++ src/components/communautaireMissionnaireGalileen/views/CommunautaireEvenements.js
@@ -0,0 +1,15 @@
+import EvenementsFutur from "../agenda/EvenementsFutur";
+import Header from "../header/Header"
+import Footer from "../footer/Footer"
+
+function CommunautaireEvenements() {
+    return(
+        <>
+            <Header />
+            <EvenementsFutur />
+            <Footer />
+        </>
+    )
+}
+
+export default CommunautaireEvenements
