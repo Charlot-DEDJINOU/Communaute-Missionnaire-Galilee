d87dd863f5cabdcde6d9870a646f814f1cc6ca26
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-10-02 09:55:43 +0100

    Page Profil

---
+++ src/components/crmaLeeaci/views/CmraLeeaciProfilUtilisateur.js
@@ -0,0 +1,15 @@
+import Header from "../header/Header"
+import Profil from "../profilUtilisateur/Profil"
+import Footer from "../footer/Footer"
+
+function CrmaLeeaciProfilUtilisateur() {
+    return(
+        <>
+            <Header />
+            <Profil />
+            <Footer />
+        </>
+    )
+}
+
+export default CrmaLeeaciProfilUtilisateur
