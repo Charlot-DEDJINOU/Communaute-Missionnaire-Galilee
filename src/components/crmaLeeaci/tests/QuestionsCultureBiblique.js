23460d74a37b05d557824d97e14dade04c36ed20
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-10-03 23:26:16 +0100

    Question CultureBibliqueé

---
+++ src/components/crmaLeeaci/tests/QuestionsCultureBiblique.js
@@ -0,0 +1,27 @@
+function QuestionsCultureBiblique() {
+    return(
+            [
+              "1- Définir : la repentance; la nouvelle naissance; le salut.",
+              "✔ Pouvez-vous expliquer l'action du Père, du Fils et du Saint-Esprit dans le processus du salut?",
+              "✔ La mort de Jésus à la croix rapporte concrètement quoi à l'humanité quand nous voulons parler des doctrines de base du salut en Jésus-Christ? Expliquez ces doctrines",
+              "2- Dans votre étude de la Bible quels sont les types de baptême que vous avez rencontré ?",
+              "✔ La Bible est prouvée scientifiquement et littéralement par L’archéologie et l'apologétique: vrai ou faux? Expliquez et défendez votre opinion dans un bref commentaire.",
+              "3- Définir: chrétien; disciple; consécration." ,
+              "✔ Quelle relation faites-vous entre un chrétien et un disciple consacré ?" ,
+              "✔ Un vrai chrétien peut-il pour des raisons ou autre se passer de leader ou conseiller spirituel ou père spirituel ? Pourquoi",
+              "4- Définir les dons spirituels; dons de ministère, dons du Saint-Esprit",
+              "5- Définir l'onction",
+              "✔ Explique la relation et la différence entre l'onction et le Saint-Esprit ? " ,
+              "✔ A quoi peut-on reconnaitre l'onction dans la vie d'un disciple de Jésus ?",
+              "6- Citez les langues de la Bible",
+              "7- Définir la théologie ",
+              "✔ Citez quelques branches de la théologie ",
+              "✔ Citez trois tendances de la théologie que vous connaissez",
+              "8- Définir : herméneutique; homilétique; exégèse.",
+              "✔ Expliquez la relation entre l'herméneutique, l'homilétique et l'exégèse ?",
+              "✔ Pourquoi l'exégèse est important pour dans l'interprétation de la Bible? appuyez-vous sur un cas d'un fait biblique"
+            ]
+    )
+}
+
+export default QuestionsCultureBiblique
