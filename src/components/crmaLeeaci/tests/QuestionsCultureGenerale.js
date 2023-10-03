e3d0792a646ce45441039d9be872b016f5dd2dd1
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-10-03 23:26:42 +0100

    Question Culture Generale

---
+++ src/components/crmaLeeaci/tests/QuestionsCultureGenerale.js
@@ -0,0 +1,14 @@
+function QuestionsCultureGenerale() {
+    return(
+            [
+              "1- Que pensez-vous de cette citation qui illustre le combat de la vie de Nelson Mandela ? Faite un bref commentaire" ,
+              "2- Est-il un panafricaniste pure au sens du thème?",
+              "3- Qu'est-ce que le panafricanisme?",
+              "4- Citez trois grandes figures du panafricanisme?",
+              "5- Que pensez-vous de la théologie de la libération de l'Afrique par l'église de l'Afrique ?",
+              "6- Que pensez-vous de l'actualité politique actuelle au Bénin ?"
+            ]
+    )
+}
+
+export default QuestionsCultureGenerale
