15e6beec6a7d3de7a24ed96ec0997d6c665be85f
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-10-11 21:33:08 +0100

    Question Niveau Français

---
+++ src/components/crmaLeeaci/tests/QuestionsNiveauFrancais.js
@@ -0,0 +1,44 @@
+function QuestionsNiveauFrancais() {
+    return(
+        [
+            {
+                "question" : "J'ai quel age ?",
+                "response" : [
+                    "16 ans" ,
+                    "17 ans",
+                    "18 ans",
+                    "19 ans",
+                ]
+            },
+            {
+                "question" : "J'ai quel age ?",
+                "response" : [
+                    "16 ans" ,
+                    "17 ans",
+                    "18 ans",
+                    "19 ans",
+                ]
+            },
+            {
+                "question" : "J'ai quel age ?",
+                "response" : [
+                    "16 ans" ,
+                    "17 ans",
+                    "18 ans",
+                    "19 ans",
+                ]
+            },
+            {
+                "question" : "J'ai quel age ?",
+                "response" : [
+                    "16 ans" ,
+                    "17 ans",
+                    "18 ans",
+                    "19 ans",
+                ]
+            }
+        ]
+    )
+}
+
+export default QuestionsNiveauFrancais
