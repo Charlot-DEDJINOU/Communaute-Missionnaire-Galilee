90417ee92f3edf62ae760f603f14130380471fed
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-10-14 19:39:56 +0100

    update

--- src/components/crmaLeeaci/tests/QuestionsNiveauFrancais.js
+++ src/components/crmaLeeaci/tests/QuestionsNiveauFrancais.js
@@ -2,14 +2,419 @@ function QuestionsNiveauFrancais() {
     return(
         [
             {
-                "question" : "J'ai quel age ?",
+                "question" : "1-J'habite ........ Mulhouse",
                 "response" : [
-                    "16 ans" ,
-                    "17 ans",
-                    "18 ans",
-                    "19 ans",
+                    "à" ,
+                    "chez",
+                    "de",
+                    "au",
                 ]
-            }
+            },
+            {
+                "question" : "2-Je vis ........ France",
+                "response" : [
+                    "à" ,
+                    "dans",
+                    "en",
+                    "chez",
+                ]
+            },
+            {
+                "question" : "3-Je viens ........ Bretagne",
+                "response" : [
+                    "à" ,
+                    "dans",
+                    "de",
+                    "chez",
+                ]
+            },
+            {
+                "question" : "4-Je vais ........ le coiffeur",
+                "response" : [
+                    "à" ,
+                    "chez",
+                    "au",
+                    "de",
+                ]
+            },
+            {
+                "question" : "5-Je suis ........ ma mère",
+                "response" : [
+                    "à" ,
+                    "chez",
+                    "de",
+                ]
+            },
+            {
+                "question" : "6-Elle adore ........ campagne",
+                "response" : [
+                    "un" ,
+                    "une",
+                    "le",
+                    "la",
+                    "l'"
+                ]
+            },
+            {
+                "question" : "7-Vous aimez ........ café ?",
+                "response" : [
+                    "un" ,
+                    "une",
+                    "le",
+                    "la",
+                    "l'"
+                ]
+            },
+            {
+                "question" : "8-Comment ........ appelez-vous ?",
+                "response" : [
+                    "t'",
+                    "vous"
+                ]
+            },
+            {
+                "question" : "9-Qui est ........ dame ?",
+                "response" : [
+                    "ce", 
+                    "cette",
+                    "cet",
+                    "une",
+                    "la",
+                    "un",
+                    "le"
+                ]
+            },
+            {
+                "question" : "10-....... épicier a ........ magasin",
+                "response" : [
+                    "ce", 
+                    "cette",
+                    "cet",
+                    "une",
+                    "la",
+                    "un",
+                    "le"
+                ]
+            },
+            {
+                "question" : "11-Elles n'ont ......... amies",
+                "response" : [
+                    "pas de",
+                    "pas d'",
+                    "pas des"
+                ]
+            },
+            {
+                "question" : "12-Ce ne sont ......... amis",
+                "response" : [
+                    "pas de",
+                    "pas d'",
+                    "pas des"
+                ]
+            },
+            {
+                "question" : "13-A la boulangerie, on vend .......... pâtisserie.",
+                "response" : [
+                    "du",
+                    "de la",
+                    "de l'",
+                    "des"
+                ]
+            },
+            {
+                "question" : "14-Le crémier vend ......... fromages",
+                "response" : [
+                    "du",
+                    "de la",
+                    "de l'" ,
+                    "des"
+                ]
+            },
+            {
+                "question" : "15-Le libraire vend ......... cartes postales",
+                "response" : [
+                    "du",
+                    "de la",
+                    "de l'",
+                    "des"
+                ]
+            },
+            {
+                "question" : "16-Ce stylo est à moi",
+                "response" : [
+                    "est-ce que c'est votre ?",
+                    "C'est son stylo ?",
+                    "à qui est ce stylo ?" ,
+                ]
+            },
+            {
+                "question" : "17-Voici les lettres ........",
+                "response" : [
+                    "urgents",
+                    "urgentes",
+                    "urgente"
+                ]
+            },
+            {
+                "question" : "18-Quelle est votre ......... adresse ?",
+                "response" : [
+                    "nouveau",
+                    "pas d'",
+                    "pas des"
+                ]
+            },
+            {
+                "question" : "19-Les trois amies ........ la radio",
+                "response" : [
+                    "écouter",
+                    "écoutes",
+                    "écoutent"
+                ]
+            },
+            {
+                "question" : "20-Tu ........ une belle chanson.",
+                "response" : [
+                    "écouter",
+                    "écoutes",
+                    "écoutent"
+                ]
+            },
+            {
+                "question" : "21-Elle ........ vu son oncle",
+                "response" : [
+                    "écouter",
+                    "écoutes",
+                    "écoutent"
+                ]
+            },
+            {
+                "question" : "22-Elle ......... vu son oncle.",
+                "response" : [
+                    "a",
+                    "est",
+                    "ont",
+                    "sont",
+                ]
+            },
+            {
+                "question" : "23-Il ......... descendu l'escalier.",
+                "response" : [
+                    "a",
+                    "est",
+                    "ont",
+                    "sont"
+                ]
+            },
+            {
+                "question" : "24-Il .......... desendu l'escalier.",
+                "response" : [
+                    "a",
+                    "est",
+                    "ont",
+                    "sont"
+                ]
+            },
+            {
+                "question" : "25-Où achète-t-on le pain ?",
+                "response" : [
+                    "demain" ,
+                    "chez le boulanger",
+                    "souvent"
+                ]
+            },
+            {
+                "question" : "26-Où achète-t-on à la gare ?",
+                "response" : [
+                    "le ticket" ,
+                    "le train",
+                    "les voyageurs"
+                ]
+            },
+            {
+                "question" : "27-Quel temps fait-il ?",
+                "response" : [
+                   "il fait beau",
+                   "c'est long",
+                   "c'est l'hiver"
+                ]
+            },
+            {
+                "question" : "28-La fille ........ vient est belle.",
+                "response" : [
+                   "qui",
+                   "que",
+                   "où",
+                   "dont"
+                ]
+            },
+            {
+                "question" : "29-La rose ......... j'achète est pour toi.",
+                "response" : [
+                   "qui",
+                   "que",
+                   "où",
+                   "dont"
+                ]
+            },
+            {
+                "question" : "30-Les villes ........... il fait plus chaud sont perpigan et Nice.",
+                "response" : [
+                   "qui",
+                   "que",
+                   "où",
+                   "dont"
+                ]
+            },
+            {
+                "question" : "31-La maison ........ tu me parles est tombée en ruine",
+                "response" : [
+                   "qui",
+                   "que",
+                   "où",
+                   "dont"
+                ]
+            },
+            {
+                "question" : "32-Les gens ......... y viennent font du cheval et vont à la plage",
+                "response" : [
+                   "qui",
+                   "que",
+                   "où",
+                   "dont"
+                ]
+            },
+            {
+                "question" : "33-Le sport .......... je préfère, c'est la planche à voile.",
+                "response" : [
+                   "qui",
+                   "que",
+                   "où",
+                   "dont"
+                ]
+            },
+            {
+                "question" : "34-Ce .......... elle a besoin, c'est le repos.",
+                "response" : [
+                   "qui",
+                   "que",
+                   "où",
+                   "dont"
+                ]
+            },
+            {
+                "question" : "35-Est-ce que .......... a téléphoné pour moi ? Non, ........",
+                "response" : [
+                   "quelque chose",
+                   "quelqu'un",
+                   "rien",
+                   "personne"
+                ]
+            },
+            {
+                "question" : "36-Je peux vous demander ......... ?",
+                "response" : [
+                   "quelque chose",
+                   "quelqu'un",
+                   "rien",
+                   "personne"
+                ]
+            },
+            {
+                "question" : "37-Désolé Monsieur , il ne reste plus ....... à louer.",
+                "response" : [
+                   "quelque chose",
+                   "quelqu'un",
+                   "rien",
+                   "personne"
+                ]
+            },
+            {
+                "question" : "38-........ journée, je tape à la machine.Le soir je rentre à la maison et j'ai envie de me coucher très tôt ...... soirs",
+                "response" : [
+                   "toutes les",
+                   "tous les",
+                   "toute la",
+                   "tout le"
+                ]
+            },
+            {
+                "question" : "39-Les côtelettes c'est bon, mais le gigot c'est ..........",
+                "response" : [
+                   "bien bon",
+                   "beaucoup bon",
+                   "meilleur"
+                ]
+            },
+            {
+                "question" : "40-Toutes ces motos sont très bonnes mais ce sont les japonaises qui sont ..........",
+                "response" : [
+                   "le mieux",
+                   "le plus bon",
+                   "les meilleures"
+                ]
+            },
+            {
+                "question" : "41-Elle s'est fait mal, elle .........",
+                "response" : [
+                   "va tomber",
+                   "tombe",
+                   "vient de tomber"
+                ]
+            },
+            {
+                "question" : "42-Il est avec ses amies, il ....... parle.",
+                "response" : [
+                   "lui",
+                   "elles",
+                   "leur",
+                   "leurs"
+                ]
+            },
+            {
+                "question" : "43-Tous les jours, ils ......... la même chanson.",
+                "response" : [
+                   "chanter",
+                   "chantent",
+                   "chantée",
+                   "chantaient"
+                ]
+            },
+            {
+                "question" : "44-Autrefois, ils ........... l'habitude de se lever tôt.",
+                "response" : [
+                   "a",
+                   "aura",
+                   "sera",
+                   "avait"
+                ]
+            },
+            {
+                "question" : "45-Il faut qu'il ........ demain.",
+                "response" : [
+                   "vient",
+                   "venir",
+                   "vienne",
+                   "viennent"
+                ]
+            },
+            {
+                "question" : "46-Je souhainte qu'il .......... heureux.",
+                "response" : [
+                   "est",
+                   "ait",
+                   "soient",
+                   "soit"
+                ]
+            },
+            {
+                "question" : "47-Je ne pense pas qu'il ......... heureux.",
+                "response" : [
+                   "peut",
+                   "puisse",
+                   "peux",
+                   "pouvoir"
+                ]
+            },
         ]
     )
 }
